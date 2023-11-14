3f1fc3e9d236faa1201cb1e993c919cc7236728d
Dima_Tatarenko <dmitriy.tatarenko@gmail.com>
2023-11-14 15:43:37 +0100

    estructura componente

---
+++ src/app/components/series/nueva-serie/nueva-serie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nueva-serie',
+  templateUrl: './nueva-serie.component.html',
+  styleUrls: ['./nueva-serie.component.css']
+})
+export class NuevaSerieComponent {
+
+}
