87324e086b6842ae46f7e77eef6c6b46e46da933
Alicia Zamorano <azpans@hotmail.com>
2023-11-14 16:23:37 +0100

    add method get all

--- src/app/core/services/series.service.ts
+++ src/app/core/services/series.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeriesService {
 
+  private httpClient = inject(HttpClient)
+
   constructor() { }
+
+
+  getAllChannels() {
+    return firstValueFrom(this.httpClient.get('https://peticiones.online/api/series'))
+  }
+
+  getByChannel() {
+
+  }
+
 }
+
+
+
+
+/* Dentro del servicio Series (core/services/series.service.ts) definimos el método getAllChannels que me devuelva todos los canales para poder visualizarlos en un select dentro de la NavBar */
