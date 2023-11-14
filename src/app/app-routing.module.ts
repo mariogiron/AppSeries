3bb209a06747a78806e967684250d7e9d3a69b59
Dima_Tatarenko <dmitriy.tatarenko@gmail.com>
2023-11-14 15:48:35 +0100

    agregada la ruta :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NuevaSerieComponent } from './components/series/nueva-serie/nueva-serie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'series/nueva', component: NuevaSerieComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
