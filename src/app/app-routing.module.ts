17f926452d032b46e350ed78aaa6360b85034462
Vanesa Martos <martosregiv@gmail.com>
2023-11-14 15:47:34 +0100

    Ruta del componente añadida.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaSeriesComponent } from './components/series/lista-series/lista-series.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'series', component: ListaSeriesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/series/lista-series/lista-series.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-series',
+  templateUrl: './lista-series.component.html',
+  styleUrls: ['./lista-series.component.css']
+})
+export class ListaSeriesComponent {
+
+}
